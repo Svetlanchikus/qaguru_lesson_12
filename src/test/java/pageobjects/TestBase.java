591c2fa531beef72e9dc1b511f2690206fac046c
sakh1 <sakh1alkhimova@gmail.com>
2023-10-03 22:28:40 +0300

    добавлена интеграция с selenoid.autotests.cloud

--- src/test/java/pageobjects/TestBase.java
+++ src/test/java/pageobjects/TestBase.java
@@ -2,6 +2,9 @@ package pageobjects;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
@@ -9,5 +12,14 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 }
