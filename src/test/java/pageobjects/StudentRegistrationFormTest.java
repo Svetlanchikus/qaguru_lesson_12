20180d935642e51f4dd8d4953c3ed8a2ed89dc83
sakh1 <sakh1alkhimova@gmail.com>
2023-10-03 23:13:02 +0300

    Добавлены описания для Allure

--- src/test/java/pageobjects/StudentRegistrationFormTest.java
+++ src/test/java/pageobjects/StudentRegistrationFormTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pageobjects.pages.StudentRegistrationFormPage;
+import pageobjects.pages.components.TableWithRegistrationResultComponent;
 
+import static io.qameta.allure.Allure.step;
 
+@DisplayName("Student registration form test")
 public class StudentRegistrationFormTest extends TestBase{
     public String firstName = "Petr";
     public String lastName = "Petrov";
@@ -23,73 +26,90 @@ public class StudentRegistrationFormTest extends TestBase{
     public String selectCity = "Delhi";
 
     StudentRegistrationFormPage studentRegistrationForm = new StudentRegistrationFormPage();
+    TableWithRegistrationResultComponent tableWithRegistrationResultComponent = new TableWithRegistrationResultComponent();
 
 
     @Test
     @Tag("remote")
     @DisplayName("Successful registration")
     void successfulRegistrationWithRequiredFieldsTest()  {
-        studentRegistrationForm
-                .openPage()
-                .checkThatTheSiteHasOpened()
-                .closeBanners()
-                .setRequiredFields(firstName,
-                        lastName,
-                        gender,
-                        number,
-                        day,
-                        month,
-                        year)
-                .clickSubmit()
-                .checkThatTheTableHasOpened()
-                .checkResultRequiredFields(firstName,
-                        lastName,
-                        gender,
-                        number,
-                        day,
-                        month,
-                        year)
-                .clickClose();
+        step("Opening a web page", () ->{
+                    studentRegistrationForm
+                            .openPage()
+                            .checkThatTheSiteHasOpened()
+                            .closeBanners();
+                });
+        step("Filling in required fields", () ->{
+            studentRegistrationForm
+                    .setRequiredFields(firstName,
+                            lastName,
+                            gender,
+                            number,
+                            day,
+                            month,
+                            year)
+                    .clickSubmit();
+        });
+        step("Checking completed fields", () ->{
+            tableWithRegistrationResultComponent
+                    .checkThatTheTableHasOpened()
+                    .checkResultRequiredFields(firstName,
+                            lastName,
+                            gender,
+                            number,
+                            day,
+                            month,
+                            year)
+                    .clickClose();
+        });
     }
 
     @Test
     @Tag("remote")
     @DisplayName("Successful registration with all fields")
     void successfulRegistrationWithAllFieldsTest() {
-        studentRegistrationForm
-                .openPage()
-                .checkThatTheSiteHasOpened()
-                .closeBanners()
-                .setAllFields(firstName,
-                        lastName,
-                        email,
-                        gender,
-                        number,
-                        day,
-                        month,
-                        year,
-                        subjects,
-                        hobbies,
-                        photo,
-                        address,
-                        selectState,
-                        selectCity)
-                .clickSubmit()
-                .checkThatTheTableHasOpened()
-                .checkResultAllFields(firstName,
-                        lastName,
-                        email,
-                        gender,
-                        number,
-                        day,
-                        month,
-                        year,
-                        subjects,
-                        hobbies,
-                        photo,
-                        address,
-                        selectState,
-                        selectCity)
-                .clickClose();
+        step("Opening a web page", () ->{
+            studentRegistrationForm
+                    .openPage()
+                    .checkThatTheSiteHasOpened()
+                    .closeBanners();
+        });
+        step("Filling in all fields", () ->{
+            studentRegistrationForm
+                    .setAllFields(firstName,
+                            lastName,
+                            email,
+                            gender,
+                            number,
+                            day,
+                            month,
+                            year,
+                            subjects,
+                            hobbies,
+                            photo,
+                            address,
+                            selectState,
+                            selectCity)
+                    .clickSubmit();
+        });
+        step("Checking completed fields", () ->{
+            tableWithRegistrationResultComponent
+                    .checkThatTheTableHasOpened()
+                    .checkResultAllFields(firstName,
+                            lastName,
+                            email,
+                            gender,
+                            number,
+                            day,
+                            month,
+                            year,
+                            subjects,
+                            hobbies,
+                            photo,
+                            address,
+                            selectState,
+                            selectCity)
+                    .clickClose();
+        });
     }
 }
--- src/test/java/pageobjects/TestBase.java
+++ src/test/java/pageobjects/TestBase.java
@@ -1,7 +1,10 @@
 package pageobjects;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -22,4 +25,10 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
     }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
 }
